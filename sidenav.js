26931363a268e7d7d949405d613e12e07c57dd1a
mela7475 <jordanm8393@gmail.com>
2021-04-14 14:05:18 -0500

    update side items to the right

--- sidenav.js
+++ sidenav.js
@@ -1,9 +1,11 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "260px";
+  document.getElementById("mySidenav").style.overflow = "initial";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.overflow = "hidden";
 }
 
 /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
@@ -18,6 +20,29 @@ for (i = 0; i < dropdown.length; i++) {
       dropdownContent.style.display = "none";
     } else {
       dropdownContent.style.display = "block";
+    }
+  });
+}
+
+var menu = document.getElementsByClassName("menu-btn");
+var j;
+
+for (j = 0; j < menu.length; j++) {
+  menu[j].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var menuContent = this.nextElementSibling;
+    if (menuContent.style.display === "block") {
+      $(this)
+        .find($(".menu-icon"))
+        .removeClass("fas fa-minus")
+        .addClass("fas fa-plus");
+      menuContent.style.display = "none";
+    } else {
+      $(this)
+        .find($(".menu-icon"))
+        .removeClass("fas fa-plus")
+        .addClass("fas fa-minus");
+      menuContent.style.display = "block";
     }
   });
 }
